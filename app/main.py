d5f3df6e5b0e769db1bad5b3b9077038afb32e7d
Dmitriy <dmitriy-avo@mail.ru>
2025-12-15 22:09:07 +0300

    сделан каркас маршрутизатора для продуктов (заглушка), подключил маршруты из products.py к основному приложению

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.routers import categories
+from app.routers import products
 
-
+# Создаём приложение FastAPI
 app = FastAPI(title="FastAPI Интернет-магазин",
               version="0.1.0")
 
+# Подключаем маршруты категорий и товаров
 app.include_router(categories.router)
+app.include_router(products.router)
+
 
 @app.get("/")
 async def root():
@@ -13,4 +17,3 @@ async def root():
     тест работоспособности API
     '''
     return {"message": "Добро пожаловать в API интернет-магазина!"}
-
