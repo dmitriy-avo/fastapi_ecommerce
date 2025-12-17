cdb38b03c555736fa85eedfd462234e83e16716b
Litoo <deman007@mail.ru>
2025-12-17 11:58:12 +0300

    создал фабрику сеансов, базовый класс для моделей.

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
 # Строка подключения для SQLite
@@ -6,3 +7,10 @@ DATABASE_URL = "sqlite:///ecommerce.db"
 
 # Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+SessionLocal = sessionmaker(bind=engine)
+
+# Определяем базовый класс для моделей
+class Base(DeclarativeBase):
+    pass
