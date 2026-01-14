154eecb4f06e37cad8930e07250e9abc4d4e23b5
Dmitriy <dmitriy-avo@mail.ru>
2026-01-14 08:43:14 +0300

    начал переводить на асинхронные запросы (сохраняя код синхронных запросов чтобы было удобно тестировать.) перевел все ручки из categories.py на асинхронные сессии

--- app/database.py
+++ app/database.py
@@ -11,6 +11,20 @@ engine = create_engine(DATABASE_URL, echo=True)
 # Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
 
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:postgres@localhost:5432/ecommerce_db"
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
 # Определяем базовый класс для моделей
 class Base(DeclarativeBase):
     pass
